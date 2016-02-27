6cc56a75cd243e690dad7211d8f8c85bfc69cee5
HL <nunavutaccountant2@gmail.com>
2016-02-28 01:14:28 +0800

    Changed it back to no default team and convo on home route. On click of team on manage teams page will always be the prev team. So put it somewhere else....

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -4,22 +4,12 @@ import {mount} from 'react-mounter';
 import MainLayout from './components/main_layout.jsx';
 import Home from './components/home.jsx';
 
-import AccountUtils from '/client/modules/core/libs/account';
-import TeamUtils from '/client/modules/core/libs/teams';
-import ConvoUtils from '/client/modules/core/libs/convos';
-
-export default function (injectDeps, {Meteor, LocalState, FlowRouter}) {
+export default function (injectDeps, {FlowRouter}) {
   const MainLayoutCtx = injectDeps(MainLayout);
 
   FlowRouter.route('/home', {
     name: 'home',
     action() {
-      const teamId = AccountUtils.getMostRecentTeamId({Meteor});
-      const convoId = AccountUtils.getMostRecentConvoId({Meteor});
-
-      TeamUtils.select({Meteor, LocalState}, teamId);
-      ConvoUtils.select({Meteor, LocalState}, convoId);
-
       mount(MainLayoutCtx, {
         content: () => (<Home />)
       });
