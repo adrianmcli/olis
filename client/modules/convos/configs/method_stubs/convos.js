0f46090b5d459556e92c4b3fad19798c080c4664
HL <nunavutaccountant2@gmail.com>
2016-03-02 17:16:56 +0800

    Updated method stub for convos

--- client/modules/convos/configs/method_stubs/convos.js
+++ client/modules/convos/configs/method_stubs/convos.js
@@ -29,7 +29,12 @@ export default function ({Meteor, Collections, Models}) {
       const convo = new Models.Convo();
       const newUserIds = [ userId, ...userIds ];
       const uniqueUserIds = R.uniq(newUserIds);
-      convo.set({name, userIds: uniqueUserIds, teamId});
+      const recentUserIds = R.takeLast(2, uniqueUserIds);
+      convo.set({
+        name,
+        userIds: uniqueUserIds,
+        recentUserIds,
+        teamId});
       convo.save();
 
       // Insert a note
