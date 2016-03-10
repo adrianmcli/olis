3e1b25e48e1d2aac36e9767a7f0f73f5180ea122
Adrian Li <li.adrianmc@gmail.com>
2016-03-10 18:32:40 +0800

    built out search modal

--- client/modules/convos/components/HeaderSearch.jsx
+++ client/modules/convos/components/HeaderSearch.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+
 import IconButton from 'material-ui/lib/icon-button';
 import ActionSearch from 'material-ui/lib/svg-icons/action/search';
-import RaisedButton from 'material-ui/lib/raised-button';
 
 import Dialog from '/client/modules/core/components/Dialog.jsx';
-import FlatButton from 'material-ui/lib/flat-button';
 
 import TextField from 'material-ui/lib/text-field';
 import CircularProgress from 'material-ui/lib/circular-progress';
@@ -15,6 +14,7 @@ export default class HeaderSearch extends React.Component {
     super(props);
     this.state = {
       open: false,
+      emptyQuery: true,
     };
   }
 
@@ -26,21 +26,104 @@ export default class HeaderSearch extends React.Component {
     this.setState({open: false});
   }
 
+  _handleOnChange() {
+    const input = this._searchField.getValue();
+    if (input === '') {
+      this.setState({emptyQuery: true});
+    } else {
+      this.setState({emptyQuery: false});
+    }
+  }
+
+  renderResults() {
+    return (
+      <div>Results</div>
+    );
+  }
+
+  renderResultsContainer() {
+    const waiting = true;
+    const renderLoading = () => {
+      return (
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingTop: '36px',
+        }}>
+          <CircularProgress />
+          <h4 style={{fontWeight: '400', lineHeight: '1.6em', color: '#00BCD4'}}>
+            Searching...
+          </h4>
+        </div>
+      );
+    };
+    return (
+      <div>
+        { waiting ? renderLoading() : this.renderResults() }
+      </div>
+    );
+  }
+
+  renderSearchDialog() {
+    const placeholderColor = '#AAA';
+    const renderEmptyPlaceholder = () => {
+      return (
+        <div style={{
+          color: placeholderColor,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingTop: '36px',
+          textAlign: 'center',
+        }}>
+          <ActionSearch
+            color={placeholderColor}
+            style={{height: '64px', width: '64px'}}
+          />
+          <h4 style={{fontWeight: '400', lineHeight: '1.6em'}}>
+            Search results will appear here <br /> when you start typing.
+          </h4>
+        </div>
+      );
+    };
+    return (
+      <Dialog
+        title="Search"
+        open={this.state.open}
+        onRequestClose={this.handleClose.bind(this)}
+        bodyStyle={{paddingTop: '0'}}
+        closeActionOnly
+        autoScrollBodyContent
+        onShow={() => {this._searchField.focus();}}
+      >
+        <div style={{
+          position: 'absolute',
+          left: '24px',
+          right: '24px',
+          top: '72px',
+          background: 'white',
+          zIndex: '1',
+        }}>
+          <TextField
+            hintText="Search messages, conversations, users, notes, and more..."
+            floatingLabelText="Search Query"
+            onChange={this._handleOnChange.bind(this)}
+            fullWidth
+            ref={ x => this._searchField = x }
+          />
+        </div>
+        <div style={{marginTop: '72px'}}>
+          { this.state.emptyQuery ? renderEmptyPlaceholder() : this.renderResultsContainer() }
+        </div>
+      </Dialog>
+    );
+  }
+
   render() {
     const iconColor = 'white';
-    const actions = [
-      <FlatButton
-        label="Cancel"
-        secondary={true}
-        onTouchTap={this.handleClose.bind(this)}
-      />,
-      <FlatButton
-        label="Submit"
-        primary={true}
-        keyboardFocused={true}
-        onTouchTap={this.handleClose.bind(this)}
-      />,
-    ];
     return (
       <div>
 
@@ -50,24 +133,7 @@ export default class HeaderSearch extends React.Component {
           </IconButton>
         </div>
 
-        {/* Search Modal Dialog */}
-        <Dialog
-          title="Search"
-          modal={false}
-          open={this.state.open}
-          onRequestClose={this.handleClose.bind(this)}
-        >
-          This is where the user will do some searching.
-          <div>
-            <TextField
-              hintText="Hint Text"
-              floatingLabelText="Floating Label Text"
-            />
-          </div>
-          <div>
-            <CircularProgress />
-          </div>
-        </Dialog>
+        { this.renderSearchDialog() }
 
       </div>
     );
