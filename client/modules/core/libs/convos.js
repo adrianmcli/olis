4b4c62686a0bd10803d4d3ea21d4dcde01a2333b
HL <nunavutaccountant2@gmail.com>
2016-03-15 16:19:37 +0800

    Fixed adding new convos. Switch convos from convo bar working.

--- client/modules/core/libs/convos.js
+++ client/modules/core/libs/convos.js
@@ -1,26 +1,9 @@
 import SectionUtils from '/client/modules/core/libs/sections';
 
 export default {
-  select({Meteor, LocalState}, convoId) {
-    const prevConvoId = LocalState.get('convoId');
-    if (prevConvoId) {
-      Meteor.call('account.setLastTimeInConvo', {convoId: prevConvoId}, (err) => {
-        if (err) { alert(err); }
-      });
-
-      Meteor.call('notifications.remove', {convoId: prevConvoId}, (err) => {
-        if (err) { alert(err); }
-      });
-    }
-
-    LocalState.set('convoId', convoId);
-    if (convoId) {
-      Meteor.call('account.setLastTimeInConvo', {convoId}, (err) => {
-        if (err) { alert(err); }
-      });
-    }
-
-    SectionUtils.releaseLock({Meteor, LocalState});
-    LocalState.set('loadMore.convoNumMsgs', null);
+  setLastTimeInConvo({Meteor}, convoId) {
+    Meteor.call('account.setLastTimeInConvo', {convoId}, (err) => {
+      if (err) { alert(err); }
+    });
   }
 };
--- client/modules/core/libs/teams.js
+++ client/modules/core/libs/teams.js
@@ -1,14 +1,6 @@
 import ConvoUtils from '/client/modules/core/libs/convos';
 
 export default {
-  // select({FlowRouter}, teamId) {
-  //   // Set last time in prev team
-  //   // Set last time in new team
-  //   // Select null convo
-
-  //   FlowRouter.go(`/team/${teamId}`);
-  // },
-
   setLastTimeInTeam({Meteor}, teamId) {
     if (teamId) {
       console.log('setLastTimeInTeam');
