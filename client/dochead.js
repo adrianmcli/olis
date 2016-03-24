13f6a2ab20c9960f37b1b986d7b17b59bada6b9e
Adrian Li <li.adrianmc@gmail.com>
2016-03-24 20:45:21 +0800

    added favicon file

--- client/dochead.js
+++ client/dochead.js
@@ -3,6 +3,10 @@ import {DocHead} from 'meteor/kadira:dochead';
 export default function () {
   DocHead.setTitle('Olis');
   DocHead.addLink({
+    rel: 'icon',
+    href: '/favicon.ico'
+  });
+  DocHead.addLink({
     href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
     rel: 'stylesheet'
   });
