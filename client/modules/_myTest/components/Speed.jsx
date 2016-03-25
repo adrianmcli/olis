a72fa55ebf35ef112f7de2ec8bb22d945693340e
HL <nunavutaccountant2@gmail.com>
2016-03-25 13:55:48 +0800

    Moved convos and msgs to own components and console logging render

--- client/modules/_myTest/components/Speed.jsx
+++ client/modules/_myTest/components/Speed.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
 
+class SpeedConvo extends React.Component {
+  render() {
+    const {convo, onClick} = this.props;
+    console.log(`SpeedConvo render ${convo.name}`);
+    return (
+      <div onClick={onClick}>{convo._id} {convo.name}</div>
+    );
+  }
+}
+
+class SpeedMsg extends React.Component {
+  render() {
+    const {msg} = this.props;
+    console.log(`SpeedMsg render ${msg.text}`);
+    return (
+      <div>{msg.text}</div>
+    );
+  }
+}
+
 export default class Speed extends React.Component {
   render() {
     const {convos, msgs, actions, convoId} = this.props;
@@ -7,16 +27,15 @@ export default class Speed extends React.Component {
       <div>
         <h1>Convos: {convoId}</h1>
         {convos.map(convo =>
-          <div
+          <SpeedConvo
             key={convo._id}
+            convo={convo}
             onClick={actions().test['speed.selectConvo'].bind(null, convo._id)}
-          >
-            {convo._id} {convo.name}
-          </div>
+          />
         )}
 
         <h2>Msgs</h2>
-        {msgs.map(msg => <div key={msg._id}>{msg.text}</div>)}
+        {msgs.map(msg => <SpeedMsg key={msg._id} msg={msg}/>)}
       </div>
     );
   }
