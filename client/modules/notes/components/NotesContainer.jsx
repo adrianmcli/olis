c50086c37d939c64dcd54f8854cf29c4b2ffa6ca
Adrian Li <li.adrianmc@gmail.com>
2016-03-27 18:07:33 +0800

    style tweaks

--- client/modules/notes/components/NotesContainer.jsx
+++ client/modules/notes/components/NotesContainer.jsx
@@ -17,10 +17,11 @@ export default class NotesContainer extends React.Component {
   render() {
     const {sections, userId, addSection, editSection, selectSection, releaseSectionLock} = this.props;
     const iconColor = 'rgba(0,0,0,0.8)';
+    const status = 'Last Updated: 23 min ago';
     return (
       <div id="notes-container">
         <div className="notes-header">
-          <div className="notes-status">Last Updated: 23 min ago</div>
+          <div className="notes-status" title={ status }>{ status }</div>
           <div className="notes-icon-bar">
             <IconButton tooltip="Share">
               <ShareIcon color={iconColor}/>
