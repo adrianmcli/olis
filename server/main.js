46b49f49c179cc74eeeac27cfb258ba8cd51df5f
HL <nunavutaccountant2@gmail.com>
2016-03-30 23:01:03 +0800

    Fixed not having the indexes built. Msg search works.

--- server/main.js
+++ server/main.js
@@ -3,9 +3,11 @@ import methods from './methods';
 import setupEmail from './email';
 import setupUserSearchSource from './SearchSource';
 import setupCloudinary from './cloudinary';
+import {buildIndexes} from '/lib/collections';
 
 publications();
 methods();
 setupEmail();
 setupUserSearchSource();
 setupCloudinary();
+buildIndexes();
--- server/publications/search.js
+++ server/publications/search.js
@@ -1,4 +1,4 @@
-import {Teams, Messages} from '/lib/collections';
+import {Teams, Messages, Convos} from '/lib/collections';
 import {Meteor} from 'meteor/meteor';
 import {check} from 'meteor/check';
 
@@ -22,21 +22,55 @@ export default function () {
       throw new Meteor.Error(SEARCH_RESULTS, 'Must be a member of a team to search it.');
     }
 
-    const msgs = Messages.find(
-      {$text: {$search: text}},
-      {
+    const convosUserIsMember = Convos.find({userIds: userId});
+    const convoIds = convosUserIsMember.map(convo => convo._id);
+
+    const _getMsgs = () => {
+      const selector = {
+        convoId: { $in: convoIds },
+        $text: { $search: text }
+      };
+      const options = {
+        fields: { score: { $meta: 'textScore' } },
+        sort: { score: { $meta: 'textScore' } }
+      };
+      return Messages.find(selector, options);
+    };
+    // console.log(_getMsgs().fetch());
+    // return _getMsgs();
+
+    const _getConvos = () => {
+      const selector = {
+        userIds: userId,
+        $text: { $search: text }
+      };
+      const options = {
+        fields: { score: { $meta: 'textScore' } },
+        sort: { score: { $meta: 'textScore' } }
+      };
+      return Convos.find(selector, options);
+    };
+    // console.log(_getConvos().fetch());
+    // return _getConvos();
+
+    const _getUsers = () => {
+      const selector = {
+        $text: {$search: text}
+      };
+      const options = {
         fields: {
-          score: {$meta: 'textScore'}
+          score: {$meta: 'textScore'},
+          username: 1,
+          emails: 1,
+          profileImageUrl: 1
         },
-        sort: {
-          score: {$meta: 'textScore'}
-        }
-      }
-    );
+        sort: { score: { $meta: 'textScore' } }
+      };
+      return Meteor.users.find(selector, options);
+    };
+    // console.log(_getUsers().fetch());
+    // return _getUsers();
 
-    return [
-      msgs
-    ];
-
+    return [ _getMsgs(), _getConvos(), _getUsers() ];
   });
 }
