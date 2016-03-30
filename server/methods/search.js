b4702b1d502fa3e24fb3fbf6920501cdcce5a801
HL <nunavutaccountant2@gmail.com>
2016-03-30 14:24:32 +0800

    Easy search working

--- server/methods/search.js
+++ server/methods/search.js
@@ -1,13 +1,15 @@
 import {Meteor} from 'meteor/meteor';
 import {Teams} from '/lib/collections';
 import {check} from 'meteor/check';
+import {IndexMsgs} from '/lib/search/easy_search';
 
 export default function () {
   const SEARCH_TEAM = 'search.team';
   Meteor.methods({
-    'search.team'({teamId}) {
+    'search.team'({teamId, searchString}) {
       check(arguments[0], {
-        teamId: String
+        teamId: String,
+        searchString: String
       });
 
       const userId = this.userId;
@@ -21,6 +23,9 @@ export default function () {
       if (!team.isUserMember(userId)) {
         throw new Meteor.Error(SEARCH_TEAM, 'Must be a member of a team to search it.');
       }
+
+      const cursor = IndexMsgs.search(searchString);
+      return cursor.fetch();
     }
   });
 }
