f81b6adaffb9cf6f5f233091ab424594d00e208d
Adrian Li <li.adrianmc@gmail.com>
2016-04-01 08:54:36 +0800

    fixed compatibility issue between meteor and react storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@kadira/storybook';
 
 function loadStories() {
-  require('../client/modules/chat/components/stories/button');
+  require('../.stories/button');
   // require as many as stories you need.
 }
 
