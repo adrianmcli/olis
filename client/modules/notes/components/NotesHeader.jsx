404e6d2c4c3a5c7be216b5341abcd246a2d33182
HL <nunavutaccountant2@gmail.com>
2016-04-12 01:13:36 +0800

    Added dropdown for adding widgets.

--- client/modules/notes/components/NotesHeader.jsx
+++ client/modules/notes/components/NotesHeader.jsx
@@ -6,11 +6,39 @@ import RedoIcon from 'material-ui/lib/svg-icons/content/redo';
 import SaveIcon from 'material-ui/lib/svg-icons/content/save';
 import ShareIcon from 'material-ui/lib/svg-icons/social/share';
 import IconButton from 'material-ui/lib/icon-button';
+import AddIcon from 'material-ui/lib/svg-icons/content/add';
+import Popover from 'material-ui/lib/popover/popover';
+import Menu from 'material-ui/lib/menus/menu';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+import EditorIcon from 'material-ui/lib/svg-icons/editor/insert-drive-file';
 
 export default class NotesHeader extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      open: false
+    };
+
+    this.handleAddWidget = this.handleAddWidget.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
+    this.handleItemTouchTap = this.handleItemTouchTap.bind(this);
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+
+  handleAddWidget(event) {
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget
+    });
+  }
+
+  handleRequestClose() {
+    this.setState({open: false});
+  }
+
+  handleItemTouchTap() {
+    this.handleRequestClose();
   }
 
   render() {
@@ -19,7 +47,7 @@ export default class NotesHeader extends React.Component {
       <div className="notes-header">
         <div className="notes-status">Last Updated: 23 min ago</div>
         <div className="notes-icon-bar">
-          <IconButton tooltip="Share">
+          {/*<IconButton tooltip="Share">
             <ShareIcon color={iconColor}/>
           </IconButton>
           <IconButton tooltip="Save to file...">
@@ -30,7 +58,30 @@ export default class NotesHeader extends React.Component {
           </IconButton>
           <IconButton tooltip="Redo">
             <RedoIcon color={iconColor}/>
+          </IconButton>*/}
+          <IconButton tooltip="Add widget" onTouchTap={this.handleAddWidget}>
+            <AddIcon color={iconColor}/>
           </IconButton>
+          <Popover
+            open={this.state.open}
+            anchorEl={this.state.anchorEl}
+            anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+            targetOrigin={{horizontal: 'left', vertical: 'top'}}
+            onRequestClose={this.handleRequestClose}
+          >
+            <Menu
+              style={{position: 'relative'}}
+              zDepth={0}
+              onItemTouchTap={this.handleItemTouchTap}
+            >
+              <MenuItem
+                primaryText="Text Editor"
+                leftIcon={<EditorIcon />}
+                // onClick={this.openTeamDirectory.bind(this)}
+              />
+            </Menu>
+        </Popover>
+
         </div>
       </div>
     );
