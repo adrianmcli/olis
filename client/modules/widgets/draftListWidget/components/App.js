3bf409c224894f99b4e550f3bfb4ca8972204275
HL <nunavutaccountant2@gmail.com>
2016-04-14 20:21:10 +0800

    Fixed list widget not having undo

--- client/modules/widgets/draftListWidget/components/App.js
+++ client/modules/widgets/draftListWidget/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import R from 'ramda';
 import { Editor, EditorState, ContentState } from 'draft-js';
 import { convertToRaw, convertFromRaw } from 'draft-js';
+import { RichUtils } from 'draft-js';
 import Paper from 'material-ui/lib/paper';
 
 export default class EditorWidget extends React.Component {
@@ -53,29 +54,69 @@ export default class EditorWidget extends React.Component {
     }
   }
 
+  _handleKeyCommand(command) {
+    const {editorState} = this.state;
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return true;
+    }
+    return false;
+  }
+
+  _toggleBlockType(blockType) {
+    this.onChange(
+      RichUtils.toggleBlockType(
+        this.state.editorState,
+        blockType
+      )
+    );
+  }
+
+  _toggleInlineStyle(inlineStyle) {
+    this.onChange(
+      RichUtils.toggleInlineStyle(
+        this.state.editorState,
+        inlineStyle
+      )
+    );
+  }
+
   _injectChanges(editorState, raw) {
     const newState = getNewState(editorState, raw);
     this.setState({editorState: newState});
   }
 
   componentWillReceiveProps(nextProps) {
-    const { data } = nextProps;
-    if (canSetStateFromProps(data)) {
+    const { data, lock, userId } = nextProps;
+
+    const isMyLock = lock && lock.userId === userId;
+    if (!isMyLock && canSetStateFromProps(data)) {
       this._injectChanges.bind(this)(this.state.editorState, data);
     }
   }
 
   render() {
+    const {lock, userId} = this.props;
+    const readOnly = lock ? lock.userId !== userId : false;
+
     return (
       <Paper style={{padding: '12px', width: '100%'}}>
         <div onClick={this.handleEditorClick}>
           <Editor
             editorState={this.state.editorState}
             onChange={this.onChange}
+            placeholder="Type something..."
             handleDrop={() => true} // Prevent other React DnD things from being dropped into it
+            readOnly={readOnly}
 
+            blockStyleFn={getBlockStyle}
+            customStyleMap={styleMap}
+            handleKeyCommand={this.handleKeyCommand}
             ref="editor"
+            spellCheck={true}
           />
+          {lock && new Date() - lock.updatedAt < 5000 ? <div>{lock.username} is typing...</div> : null}
         </div>
       </Paper>
     );
@@ -84,6 +125,23 @@ export default class EditorWidget extends React.Component {
 EditorWidget.defaultProps = {
   lock: undefined
 };
+
+// Custom overrides for "code" style.
+const styleMap = {
+  CODE: {
+    backgroundColor: 'rgba(0, 0, 0, 0.05)',
+    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
+    fontSize: 16,
+    padding: 2,
+  },
+};
+
+function getBlockStyle(block) {
+  switch (block.getType()) {
+    case 'blockquote': return 'RichEditor-blockquote';
+    default: return null;
+  }
+}
 
 function canSetStateFromProps(data) {
   const hasData = data && !R.isEmpty(data);
@@ -94,58 +152,11 @@ function canSetStateFromProps(data) {
   return hasData && hasAllKeys;
 }
 
-function getSelectedBlocks(editorState) {
-  const selectionState = editorState.getSelection();
-
-  const hasFocus = selectionState.getHasFocus();
-  const start = selectionState.getStartKey();
-  const end = selectionState.getEndKey();
-
-  if (start && hasFocus) {
-    const contentState = editorState.getCurrentContent();
-    const blockArray = contentState.getBlocksAsArray();
-
-    /* eslint-disable curly */
-    let index = {start: null, end: null};
-    blockArray.map((block, i) => {
-      if (block.getKey() === start) index.start = i;
-      if (block.getKey() === end) index.end = i;
-    }); /* eslint-enable */
-
-    return blockArray.filter((_, i) => i >= index.start && i <= index.end);
-  }
-  return [];
-}
-
-function mergeBlockArrays(editorState, newBlocks, selectedBlocks) {
-  const contentState = editorState.getCurrentContent();
-
-  return newBlocks.map( newBlock => {
-    const key = newBlock.getKey();
-    const selectedBlock = contentState.getBlockForKey(key);
-    const isSelected = R.contains(key, selectedBlocks.map(x => x.getKey()));
-
-    return isSelected ? selectedBlock : newBlock;
-  });
-}
-
 function getNewState(editorState, raw) {
-  // Getting current data
-  const currentSelectionState = editorState.getSelection();
-
-  // Get the two block arrays and then merge them to form a new one
-  const newContentBlocks = convertFromRaw(raw);       // from server
-  const selectedBlocks = getSelectedBlocks(editorState);    // from user selection
-  const newBlockArray = mergeBlockArrays(
-    editorState, newContentBlocks, selectedBlocks
-  );
+  const newContentBlocks = convertFromRaw(raw); // from server
 
   // Wrapping it all back up into an EditorState object
-  const newContentState = ContentState.createFromBlockArray(newBlockArray);
+  const newContentState = ContentState.createFromBlockArray(newContentBlocks);
   const newEditorState = EditorState.push(editorState, newContentState);
-
-  const hasFocus = currentSelectionState.getHasFocus();
-  const maintainSelection = hasFocus ? EditorState.forceSelection : EditorState.acceptSelection;
-  const newState = maintainSelection(newEditorState, currentSelectionState);
-  return newState;
+  return newEditorState;
 }
