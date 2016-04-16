8d6ccc5110cfb16341603e7cae0094fc9b551f19
HL <nunavutaccountant2@gmail.com>
2016-04-16 17:47:22 +0800

    Using EditorWidget as the base for ListWidget

--- client/modules/widgets/index.js
+++ client/modules/widgets/index.js
@@ -3,9 +3,9 @@ import methodStubs from './configs/method_stubs';
 
 import DraggableWidget from './draggableWidget/components/DraggableWidget.jsx';
 import EditorWidget from './editorWidget/components/EditorWidget.jsx';
+import ListWidget from './draftListWidget/components/App';
 import TodoWidget from './todoWidget/components/App';
 import VoteWidget from './voteWidget/components/App';
-import ListWidget from './draftListWidget/components/App';
 
 export default {
   actions,
