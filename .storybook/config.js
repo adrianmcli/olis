094d67ee0f9369ffb3cdc29e0284ded04f31499e
HL <nunavutaccountant2@gmail.com>
2016-04-18 14:03:12 +0800

    Fixed story book for list widget

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,10 @@
 import { configure } from '@kadira/storybook';
 
 function loadStories() {
-  require('../client/modules/widgets/todoWidget/components/stories/App');
-  require('../client/modules/widgets/voteWidget/components/stories/App');
+  // require('../client/modules/widgetTodo/components/stories/App');
+  // require('../client/modules/widgetVote/components/stories/App');
   require('../client/modules/widgets/listWidget/components/stories/App');
-  require('../client/modules/widgets/draftListWidget/components/stories/App');
+  // require('../client/modules/widgetDraftList/components/stories/App');
 }
 
 configure(loadStories, module);
