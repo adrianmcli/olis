dfc8a3d8f07e10a2b099478f0c0a96ba0049c054
Adrian Li <li.adrianmc@gmail.com>
2016-04-18 17:12:54 +0800

    styled system message item

--- client/modules/chat/components/SystemMessageItem.jsx
+++ client/modules/chat/components/SystemMessageItem.jsx
@@ -4,7 +4,25 @@ export default class SystemMessageItem extends React.Component {
   render() {
     const {content} = this.props;
     return (
-      <div>{content}</div>
+      <div style={getStyles().container}>
+        <div style={getStyles().text}>{content}</div>
+      </div>
     );
   }
+}
+
+function getStyles() {
+  return {
+    container: {
+      padding: '6px',
+    },
+    text: {
+      textAlign: 'center',
+      padding: '6px',
+      background: 'rgba(255,255,255,0.5)',
+      color: 'grey',
+      borderRadius: '2px',
+      fontWeight: '300',
+    },
+  };
 }
