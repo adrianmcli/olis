ff74bd98fdc6e9c0a495d10b6dce3d282ab0b83d
Adrian Li <li.adrianmc@gmail.com>
2016-04-19 15:22:37 +0800

    switched chat input to draft js editor, non functioning

--- client/modules/chat/components/ChatInput.jsx
+++ client/modules/chat/components/ChatInput.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import PureRenderMixin from 'react-addons-pure-render-mixin';
-
-import TextField from 'material-ui/lib/text-field';
+import {Editor, EditorState} from 'draft-js';
 
 export default class ChatInput extends React.Component {
   constructor(props) {
     super(props);
-    // this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+    this.focus = () => this.refs.editor.focus();
   }
 
   handleEnterKeyDown(e) {
@@ -24,16 +24,15 @@ export default class ChatInput extends React.Component {
   }
 
   render() {
+    const {editorState} = this.state;
     return (
       <div id="chat-input">
-        <div className="chat-input-container">
-          <TextField
-            hintText="Type your message here"
-            multiLine={true}
-            rows={1}
-            rowsMax={10}
-            style={{transition: 'none', width: '90%', margin: '8px'}}
-            onEnterKeyDown={this.handleEnterKeyDown.bind(this)}
+        <div className="chat-input-container" onClick={this.focus}>
+          <Editor
+            editorState={editorState}
+            onChange={this.onChange}
+            placeholder="Type your message here..."
+            ref="editor"
           />
         </div>
       </div>
