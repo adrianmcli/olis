103c741c41a7237bcdd6e4ac27be25ea55e22199
HL <nunavutaccountant2@gmail.com>
2016-04-21 12:47:36 +0800

    NoAccount links work

--- client/modules/account/components/onboarding/NoAccount.jsx
+++ client/modules/account/components/onboarding/NoAccount.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 export default class NoAccount extends React.Component {
   render() {
     return (
-      <div>NoAccount</div>
+      <div>
+        <h2>Register</h2>
+        <a href="/register">Create a new account.</a>
+
+        <h2>Has someone invited you to Olis alredy?</h2>
+        <a href="/find-my-team">Find My Team</a>
+      </div>
     );
   }
 }
