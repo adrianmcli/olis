15dfa3754481091e074c97b38e0ad8cb479f8215
Adrian Li <li.adrianmc@gmail.com>
2016-04-22 03:45:31 +0800

    fixed some style issues

--- client/modules/widgetTodo/components/TodoItem.js
+++ client/modules/widgetTodo/components/TodoItem.js
@@ -44,7 +44,6 @@ export default class TodoItem extends React.Component {
           this.state.hovering ?
             <IconButton
               onClick={removeTask.bind(null, id)}
-              tooltip="Remove Task"
               style={styles.removeBtn}
             >
               <RemoveIcon />
