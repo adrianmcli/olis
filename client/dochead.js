08e8d00ea36d69a1245828bf1fc325ad5ddfef60
HL <nunavutaccountant2@gmail.com>
2016-04-21 17:35:32 +0800

    Fixed old favicon on clearing the favicon notification

--- client/dochead.js
+++ client/dochead.js
@@ -1,11 +1,18 @@
 import {DocHead} from 'meteor/kadira:dochead';
+import FaviconNotification from 'favicon-notification';
+
+const favicon = '/favicon.ico?v=1.0.2';
 
 export default function () {
   DocHead.setTitle('Olis');
   DocHead.addLink({
     rel: 'icon',
-    href: '/favicon.ico?v=1.0.1'
+    href: favicon
   });
+  FaviconNotification.init({
+    url: favicon,
+  });
+    
   DocHead.addLink({
     href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
     rel: 'stylesheet'
