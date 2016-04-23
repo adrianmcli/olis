dde4e3184e44616822432d49522f3ed78d5c85cb
Adrian Li <li.adrianmc@gmail.com>
2016-04-23 19:50:35 +0800

    added enter key down handler

--- client/modules/teams/components/HeaderMenuItems/InviteToTeam.jsx
+++ client/modules/teams/components/HeaderMenuItems/InviteToTeam.jsx
@@ -33,6 +33,7 @@ export default class InviteToTeam extends React.Component {
           hintText="your.name@example.com"
           floatingLabelText="Email"
           ref={ x => this._textField = x }
+          onEnterKeyDown={this.handleSubmit.bind(this)}
           fullWidth
           // onChange={this.handleChange.bind(this, ref, i)}
           // errorText={this.state.showErrorText[i] ? 'Enter a proper email.' : null}
