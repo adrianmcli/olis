2f072703c2160b9a491e17fcdbdea62d959242ba
HL <nunavutaccountant2@gmail.com>
2016-05-03 18:05:46 +0800

    Added node-gcm package, method to set GCMToken for a user.

--- server/methods/notifications.js
+++ server/methods/notifications.js
@@ -3,7 +3,7 @@ import {Notifications} from '/lib/collections';
 import {check} from 'meteor/check';
 
 export default function () {
-  const NOTIFICATIONS_REMOVE = 'notifications.remove';
+  const REMOVE = 'notifications.remove';
   Meteor.methods({
     'notifications.remove'({convoId}) {
       check(arguments[0], {
@@ -12,15 +12,34 @@ export default function () {
 
       const userId = this.userId;
       if (!userId) {
-        throw new Meteor.Error(NOTIFICATIONS_REMOVE, 'Must be logged in to remove notifications.');
+        throw new Meteor.Error(REMOVE, 'Must be logged in to remove notifications.');
       }
       const notif = Notifications.findOne({userId, convoId});
       if (notif) {
         if (!notif.belongsToUser(userId)) {
-          throw new Meteor.Error(NOTIFICATIONS_REMOVE, 'Can only remove notifications that belong to yourself.');
+          throw new Meteor.Error(REMOVE, 'Can only remove notifications that belong to yourself.');
         }
         else { notif.remove(); }
       }
-    }
+    },
+  });
+
+  const SET_GCM_TOKEN = 'notifications.set.GCMToken';
+  Meteor.methods({
+    'notifications.set.GCMToken'({token}) {
+      check(arguments[0], {
+        token: String,
+      });
+
+      const userId = this.userId;
+      if (!userId) {
+        throw new Meteor.Error(SET_GCM_TOKEN, 'Must be logged in to set GCM token.');
+      }
+
+      Meteor.users.update(userId, {
+        $set: { GCMToken: token},
+      });
+      console.log(`${userId} GCMToken = ${token}`);
+    },
   });
 }
